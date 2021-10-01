7864af4bf8a3c35880489e053311724dde967b9f
brianminson1 <78385268+brianminson1@users.noreply.github.com>
2021-10-01 12:39:53 -0500

    Assessment 4

---
+++ confirmation.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+<head>
+<title>Confirmation Page of Web Form</title>
+</head>
+
+<h1>Confirmation Page of Customer Info</h1>
+
+<p>Thank you for submitting this form. 
+
+<p>We have successfully received it. 
+
+<p>Below is a summary of the information you provided.<br><br>
+
+<!-- confirmation.php will display the below message after successful validation -->
+
+<?php
+
+echo "You form is successfully submitted.\nThank You for registeration.";
+
+?>
+</body>
+</html>
